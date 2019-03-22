22ef68ed05fcb602f4e982cf218741bf547925c3
John Thornton <jthornton@gnipsel.com>
2019-03-22 10:17:20 -0500

    update setup to copy config and themes

--- setup.py
+++ setup.py
@@ -3,6 +3,24 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+DATA_DIRS = [
+    ('~/linuxcnc/configs/mill.touch', 'configs'),
+    ('~/linuxcnc/configs/mill.touch/themes', 'themes'),
+    ]
+
+def data_files_from_dirs(data_dirs):
+    data_files = []
+    for dest_dir, source_dir in data_dirs:
+        dest_dir = os.path.expanduser(dest_dir)
+        for root, dirs, files in os.walk(source_dir):
+            root_files = [os.path.join(root, i) for i in files]
+            dest = os.path.join(dest_dir, os.path.relpath(root, source_dir))
+            data_files.append((dest, root_files))
+
+    return data_files
+
+data_files.extend(data_files_from_dirs(DATA_DIRS))
+
 setup(
     name="mill_touch",
     version="0.0.1",
